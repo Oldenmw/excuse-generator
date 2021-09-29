a2f746688732ea6c231edbc99d25c0912a316561
Matt Oldenburg <theoldenmw@gmail.com>
2021-09-29 02:41:28 -0400

    excuses wont show twice in a row

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
 var button = document.getElementById("btn");
 var excuseArr = ["We played nano blade into 6 man!", "I charged them off the map!", "We wanted to practice different comps!", "We weren't on main roles!"]
+var previousExcuse = excuseArr.length + 1;
 
-
-var displayExcuse = function() {
+var displayExcuse = function(index) {
     var excuseEl = document.querySelector("#excuse");
     excuseEl.textContent = "";
+    excuseEl.textContent = excuseArr[index];
+}
+var randomExcuse = function() {
     var random = Math.floor(Math.random() * (excuseArr.length));
-    excuseEl.textContent = excuseArr[random];
-    console.log(random)
+    if (random === previousExcuse) {
+        randomExcuse();
+    } else {
+        previousExcuse = random;
+        displayExcuse(random);
+    }
 }
 
-button.addEventListener("click", () => displayExcuse());
+button.addEventListener("click", () => randomExcuse());
